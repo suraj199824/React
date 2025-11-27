6d06949bcb0f62d6a99bf9a96f1f32925c5209c6
Suraj  Dev <surajdev@LTP0364.local>
2025-11-27 20:15:01 +0530

    pushibng to test server

---
+++ App.js
@@ -0,0 +1,14 @@
+const heading = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child"}, [
+        React.createElement("h1", {}, "This is h1 tag"),
+        React.createElement("h2", {}, "This is h2 tag")
+    ]),
+    React.createElement("div", {id: "child"}, [
+        React.createElement("div", {id: "title"}, "Namaste React!!"),
+        React.createElement("h1", {}, "This is h1 tag"),
+        React.createElement("h2", {}, "This is h2 tag")
+    ])
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
