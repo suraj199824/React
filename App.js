0ed7d0d9a84e6117267f5135212486790f4aebc8
Suraj  Dev <surajdev@LTP0364.local>
2025-11-30 21:25:14 +0530

    push to git

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
-const heading = React.createElement("div", {id: "parent"}, [
-    React.createElement("div", {id: "child"}, [
-        React.createElement("h1", {}, "This is h1 tag"),
-        React.createElement("h2", {}, "This is h2 tag")
-    ]),
-    React.createElement("div", {id: "child"}, [
-        React.createElement("div", {id: "title"}, "Namaste React!!"),
-        React.createElement("h1", {}, "This is h1 tag"),
-        React.createElement("h2", {}, "This is h2 tag")
-    ])
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement(
+      "div",
+      { id: "child", key: "child-1" },
+      [
+        React.createElement("h1", { key: "h1-1" }, "This is h1 tag"),
+        React.createElement("h2", { key: "h2-1" }, "This is h2 tag")
+      ]
+    ),
+    React.createElement(
+      "div",
+      { id: "child", key: "child-2" },
+      [
+        React.createElement("div", { id: "title", key: "title" }, "Namaste React!!"),
+        React.createElement("h1", { key: "h1-2" }, "This is h1 tag"),
+        React.createElement("h2", { key: "h2-2" }, "This is h2 tag")
+      ]
+    )
+  ]
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # React
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm. - Written in C++
+- Caching - faster Builds
+- Image Optimization
+- Minification
+- Bundle
+- Compress
+- Error Handling
+- Code Splitting
+- Diagnosic
+- Remove unused code for you
